0ed21601da180557af5d9e8f1a8c24b99efe22c0
Jelmer Tjoelker <110289451+tjoelker@users.noreply.github.com>
2024-03-16 16:32:45 +0100

    Created a class

--- index.php
+++ index.php
@@ -4,3 +4,52 @@ require 'functions.php';
 require 'router.php';
 
 pre_dump($_SERVER['REQUEST_URI']);
+
+// Connect to MySQL database:
+
+class Release 
+{
+	public ?int $year;
+	public ?string $title;
+	public ?string $artist;
+	public ?array $tracklist;
+
+	public function createTable(): void
+	{
+		echo "<table>";
+		echo "<tr><th>Artist</th><td>{$this->artist}</td></tr>";
+		echo "<tr><th>Title</th><td>{$this->title}</td></tr>";
+		echo "<tr><th>Year</th><td>{$this->year}</td></tr>";
+		echo "<tr><th>Tracklist</th><td>";
+		echo self::printTracklist($this->tracklist);
+		echo "</td></tr>";
+		echo "</table>";
+	}
+
+	private function printTracklist($list): void
+	{
+		echo "<ol>";
+		foreach ($list as $title) {
+			echo "<li>{$title}</li>";
+		}
+		echo "</ol>";
+	}
+}
+
+$album = new Release;
+$album->year = 2018;
+$album->title = 'Portrait with Firewood';
+$album->artist = 'DjRUM';
+$album->tracklist = [
+	'Unblocked',
+	'Waters Rising (feat. Lola Empire)',
+	'Creature Pt. 1',
+	'Creature Pt. 2',
+	'Sex',
+	'Blue Violet',
+	'Sparrows (feat. Lola Empire)',
+	'Showreel Pt. 3',
+	'Blood In My Mouth',
+];
+
+$album->createTable();
